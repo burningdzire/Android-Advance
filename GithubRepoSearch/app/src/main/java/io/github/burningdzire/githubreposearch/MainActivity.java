c57c51d7a9cf725aff00f480ce08fef83be73f81
burningdzire <mohit.sharma@tatahealth.com>
2018-07-17 22:51:21 +0530

    T02.02-AddingMenu

--- GithubRepoSearch/app/src/main/java/io/github/burningdzire/githubreposearch/MainActivity.java
+++ GithubRepoSearch/app/src/main/java/io/github/burningdzire/githubreposearch/MainActivity.java
@@ -1,12 +1,17 @@
 package io.github.burningdzire.githubreposearch;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String LOG_TAG = "MainActivity";
     private EditText mSearchBoxEditText;
     private TextView mUrlDisplayTextView;
     private TextView mSearchResultsTextView;
@@ -19,5 +24,23 @@ public class MainActivity extends AppCompatActivity {
         mSearchBoxEditText = (EditText) findViewById(R.id.et_search_box);
         mUrlDisplayTextView = (TextView) findViewById(R.id.tv_url_display);
         mSearchResultsTextView = (TextView) findViewById(R.id.tv_github_search_result_json);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemThatwasSelected = item.getItemId();
+        if (itemThatwasSelected == R.id.action_search){
+            Context context = MainActivity.this;
+            String Message = "Search Clicked";
+            Toast.makeText(context, Message, Toast.LENGTH_LONG).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
