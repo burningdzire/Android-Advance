29216a05fa86ecfb76465afec36526266a6ca8eb
burningdzire <mohit.sharma@tatahealth.com>
2018-07-17 15:51:59 +0530

    DisplayToyList

--- FavoriteToys/app/src/main/java/io/github/burningdzire/favoritetoys/MainActivity.java
+++ FavoriteToys/app/src/main/java/io/github/burningdzire/favoritetoys/MainActivity.java
@@ -2,12 +2,23 @@ package io.github.burningdzire.favoritetoys;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView mToysListTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToysListTextView = (TextView) findViewById(R.id.tv_toy_names);
+        String [] toyNames = ToyBox.getToyNames();
+        StringBuilder toyName = new StringBuilder();
+        for (int i = 0; i < toyNames.length; i++)
+        {
+            toyName.append(toyNames[i] + "\n\n\n");
+        }
+        mToysListTextView.setText(toyName.toString());
     }
 }
