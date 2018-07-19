e22d23b993a84b7b46a7cabe0963900cc2e9b40f
burningdzire <mohit.sharma@tatahealth.com>
2018-07-19 11:27:46 +0530

    S02.03-VisualPolish

--- Sunshine/app/src/main/java/io/github/burningdzire/sunshine/MainActivity.java
+++ Sunshine/app/src/main/java/io/github/burningdzire/sunshine/MainActivity.java
@@ -5,10 +5,16 @@ import android.support.v4.content.res.FontResourcesParserCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.zip.Inflater;
 
 import io.github.burningdzire.sunshine.data.SunshinePreferences;
 import io.github.burningdzire.sunshine.utilities.NetworkUtils;
@@ -17,6 +23,8 @@ import io.github.burningdzire.sunshine.utilities.OpenWeatherJsonUtils;
 public class MainActivity extends AppCompatActivity {
 
     private TextView mWeatherTextView;
+    private TextView mErrorMessageDisplay;
+    private ProgressBar mLoadingProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,24 +32,45 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_forecast);
 
         mWeatherTextView = (TextView) findViewById(R.id.tv_weather_data);
+        mErrorMessageDisplay = (TextView) findViewById(R.id.tv_error_message_display);
+        mLoadingProgress = (ProgressBar) findViewById(R.id.pb_loading_indicator);
+
         loadWeatherData();
     }
 
 
     private void loadWeatherData() {
+        showWeatherDateView();
         String location = SunshinePreferences.getPreferredWeatherLocation(this);
         new FetchWeatherTask().execute(location);
     }
 
-    public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
-        @Override
-        protected String[] doInBackground(String... locations) {
+    private void showWeatherDateView() {
+        mErrorMessageDisplay.setVisibility(View.INVISIBLE);
+        mWeatherTextView.setVisibility(View.VISIBLE);
+    }
 
-            if (locations.length == 0) {
+    private void showErrorMessage() {
+        mErrorMessageDisplay.setVisibility(View.VISIBLE);
+        mWeatherTextView.setVisibility(View.INVISIBLE);
+    }
+
+    public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mLoadingProgress.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected String[] doInBackground(String... params) {
+
+            if (params.length == 0) {
                 return null;
             }
 
-            String location = locations[0];
+            String location = params[0];
             URL weatherRequestUrl = NetworkUtils.buildUrl(location);
 
             try {
@@ -61,11 +90,37 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(String[] weatherData) {
+            mLoadingProgress.setVisibility(View.INVISIBLE);
+
             if (weatherData != null) {
+                showWeatherDateView();
+
                 for (String weatherString : weatherData) {
                     mWeatherTextView.append((weatherString) + "\n\n\n");
                 }
+            } else {
+                showErrorMessage();
             }
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.forecast, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_refresh) {
+            mWeatherTextView.setText("");
+            loadWeatherData();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
