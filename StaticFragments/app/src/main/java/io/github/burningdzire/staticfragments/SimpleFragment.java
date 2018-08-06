76b11cf197747f52169ac167c3a0a2b306c138a3
burningdzire <mohit.sharma@tatahealth.com>
2018-08-06 14:44:48 +0530

    Static Fragment

---
+++ StaticFragments/app/src/main/java/io/github/burningdzire/staticfragments/SimpleFragment.java
@@ -0,0 +1,56 @@
+package io.github.burningdzire.staticfragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SimpleFragment extends Fragment {
+
+
+    private static  final int YES = 1;
+    private static  final int NO = 0;
+    public SimpleFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, final ViewGroup container,
+                             Bundle savedInstanceState) {
+        final View rootView = inflater.inflate(R.layout.fragment_simple, container, false);
+        final RadioGroup radioGroup = rootView.findViewById(R.id.radio_group);
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
+                View radioButton = radioGroup.findViewById(checkedId);
+                int index = radioGroup.indexOfChild(radioButton);
+
+                TextView textView = (TextView) rootView.findViewById(R.id.tv_fragment_header);
+                switch (index) {
+                    case YES:
+                        textView.setText(R.string.yes_message);
+                        break;
+                    case NO:
+                        textView.setText(R.string.no_message);
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+        });
+        return rootView;
+    }
+
+}
